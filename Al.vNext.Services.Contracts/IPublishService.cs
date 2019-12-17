f839c4687657b563dfb089ab78f7704c8d60e85e
GavinHome <yangxiaoming512@yeah.net>
2019-12-17 17:34:29 +0800

    add masstrait

---
+++ Al.vNext.Services.Contracts/IPublishService.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------------------
+// <copyright file="IPublishService.cs" company="Al.vNext">
+//     Copyright Al.vNext. All rights reserved.
+// </copyright>
+// <author>??</author>
+// <date>2019/12/17 14:27:51</date>
+// <description></description>
+//-----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Al.vNext.ViewModel.Auth;
+
+namespace Al.vNext.Services.Contracts
+{
+    public interface IPublishService
+    {
+        Task<bool> Send<T>(T data, string queneName);
+    }
+}
---
+++ Al.vNext.Services.Implement/PublishService.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------------------
+// <copyright file="PublishService.cs" company="Al.vNext">
+//     Copyright Al.vNext. All rights reserved.
+// </copyright>
+// <author>??</author>
+// <date>2019/12/17 14:27:51</date>
+// <description></description>
+//-----------------------------------------------------------------------------------
+
+using Al.vNext.Services.Contracts;
+using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace Al.vNext.Services.Implement
+{
+    public class PublishService : IPublishService
+    {
+        private readonly ISendEndpointProvider _sendEndpointProvider;
+
+        public PublishService(ISendEndpointProvider sendEndpointProvider)
+        {
+            _sendEndpointProvider = sendEndpointProvider;
+        }
+
+        public async Task<bool> Send<T>(T data, string queneName)
+        {
+            var endpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri($"rabbitmq://localhost/{queneName}"));
+            await endpoint.Send(data);
+            return true;
+        }
+    }
+}
