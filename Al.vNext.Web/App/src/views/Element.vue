a5f37c7b935898fdd45969be64dae482533ae324
GavinHome <yangxiaoming512@yeah.net>
2019-12-17 18:00:24 +0800

    add element for App project

---
+++ Al.vNext.Web/App/src/views/Element.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="ele">
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Button, Select } from 'element-ui';
+Vue.use(Button)
+
+export default {
+  name: "Element",
+  components: {
+  }
+};
+</script>
